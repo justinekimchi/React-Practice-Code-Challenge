627452044b3d4bc92a85102eacc09dbe2ecbbff6
justinekimchi <justine.kim.manaloto@gmail.com>
2020-12-18 06:43:31 -0600

    commit tree 87843c0b399a0cd7beb4f317c7c1ce75e422bd51

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,20 +1,74 @@
-import React, { Component } from 'react';
-import SushiContainer from './containers/SushiContainer';
-import Table from './containers/Table';
+import React, { Component } from "react";
+import SushiContainer from "./containers/SushiContainer";
+import Table from "./containers/Table";
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis";
 
 class App extends Component {
+    state = {
+        sushis: [],
+        eatenSushis: [],
+        index: 0,
+        money: 100,
+    };
 
-  render() {
-    return (
-      <div className="app">
-        <SushiContainer />
-        <Table />
-      </div>
-    );
-  }
+    componentDidMount() {
+        fetch(API)
+            .then((res) => res.json())
+            .then((data) => this.setState({ sushis: data }));
+    }
+
+    handleNextSushi = () => {
+        // console.log("clicked...");
+        this.state.index === 96
+            ? this.setState({ index: 0 })
+            : this.setState({ index: this.state.index + 4 });
+    };
+    //handleNextSushi could also be written as a regular if/else statement:
+    // if(this.state.index===96){
+    // this.setState({
+    //     index:0
+    // })}
+    // }else{this.setState({
+    // index:this.state.index +4
+    // })
+    // }
+
+    handleEatenSushi = (sushi) => {
+        if (
+            !this.state.eatenSushis.includes(sushi) &&
+            this.state.money >= sushi.price
+        ) {
+            this.setState({
+                eatenSushis: [...this.state.eatenSushis, sushi],
+                money: this.state.money - sushi.price,
+            });
+        }
+    };
+
+    render() {
+        let sushis = this.state.sushis.slice(
+            this.state.index,
+            this.state.index + 4
+        );
+        // console.log(this.state.eatenSushis);
+        return (
+            <div className="app">
+                <SushiContainer
+                    sushis={sushis}
+                    handleNextSushi={this.handleNextSushi}
+                    handleEatenSushi={this.handleEatenSushi}
+                    eatenSushi={this.state.eatenSushis}
+                />
+
+                <Table
+                    money={this.state.money}
+                    eatenSushi={this.state.eatenSushis}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,23 +1,25 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
 const Sushi = (props) => {
-  return (
-    <div className="sushi">
-      <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          false ?
-            null
-          :
-            <img src={/* Give me an image source! */ } width="100%" />
-        }
-      </div>
-      <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
-      </h4>
-    </div>
-  )
-}
+    // console.log(props.sushi);
+    return (
+        <div className="sushi">
+            <div
+                className="plate"
+                onClick={() => props.handleEatenSushi(props.sushi)}
+            >
+                {
+                    /* Tell me if this sushi has been eaten! */
+                    props.check ? null : (
+                        <img src={props.sushi.img_url} alt="" width="100%" />
+                    )
+                }
+            </div>
+            <h4 className="sushi-details">
+                {props.sushi.name} - ${props.sushi.price}
+            </h4>
+        </div>
+    );
+};
 
-export default Sushi
+export default Sushi;
